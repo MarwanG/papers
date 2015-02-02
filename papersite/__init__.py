0b4b569287be133240b3735d7bd9bde96059919c
Sergey Kirgizov <sergey.kirgizov@u-bourgogne.fr>
2015-02-02 14:14:51 +0100

    cosmetic changes. wsgi config

--- papersite/__init__.py
+++ papersite/__init__.py
@@ -1,10 +1,10 @@
-import papersite.config
+from papersite.config import appc
 from flask import Flask
 
 app = Flask(__name__,
             static_folder='../static',
             template_folder='../templates')
-app.config.update(papersite.config.appc)
+app.config.update(appc)
 
 import papersite.db
 import papersite.user
